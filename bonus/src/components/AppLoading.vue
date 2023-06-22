7d642a29c1cee81b18ee8cc7e04c2b2995ba1524
EugeniuPeruzzi <jaka1997@live.it>
2023-06-22 18:20:08 +0200

    error

---
+++ bonus/src/components/AppLoading.vue
@@ -0,0 +1,34 @@
+<script>
+export
+
+
+</script>
+<template>
+    <div>
+        <div class="loader-content">
+            <div class="custom-loader"></div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.loader-content {
+    width: 300px;
+    height: 300px;
+}
+
+.custom-loader {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    border: 16px solid;
+    border-color: #766DF4 #0000;
+    animation: s1 0.5s infinite;
+}
+
+@keyframes s1 {
+    to {
+        transform: rotate(.5turn)
+    }
+}
+</style>
--- bonus/src/store.js
+++ bonus/src/store.js
@@ -4,5 +4,6 @@ export const store = reactive({
     // il link che ho trovato tramite postman
     apiUrl: 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=12',
     // array vuoto da popolare
-    pokeDex: []
+    pokeDex: [],
+    loading: false,
 });
