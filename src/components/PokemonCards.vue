3d119b06bbf676a9a115d726e23e7d6855e1a8dd
EugeniuPeruzzi <jaka1997@live.it>
2023-06-24 01:21:10 +0200

    computed fixed

--- src/components/PokemonCards.vue
+++ src/components/PokemonCards.vue
@@ -15,7 +15,7 @@ export default {
 
 <template>
     <!-- creo la card con i dati richiesti estrapolandoli dal array -->
-    <div class="kard my-3 rounded-4" :style="'background-color lowerCaseColors'">
+    <div class="kard my-3 rounded-4" :style="{ 'background-color': lowerCaseColors }">
         <div class="kard-content d-flex align-items-center justify-content-center flex-column p-4">
             <div class="img-container">
                 <img :src="myPokemonList.imageUrl" alt="" class="rounded-circle">
@@ -28,37 +28,9 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-// .green {
-//     background-color: #DEFDE0 !important;
-// }
-
-// .red {
-//     background-color: #FDDFDF !important;
-// }
-
-// .blue {
-//     background-color: #DEF3FD !important;
-// }
-
-// .bug {
-//     background-color: #8cee4173 !important;
-// }
-
-// .normal {
-//     background-color: #D7D1BD !important;
-// }
-
-// .electric {
-//     background-color: #F8BC28 !important;
-// }
-
-// .dark {
-//     background-color: #806155 !important;
-// }
-
-// .poison {
-//     background-color: #8E498D !important;
-// }
+.white {
+    color: #fff;
+}
 
 img {
     height: 170px;
